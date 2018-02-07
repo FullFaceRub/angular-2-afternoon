41e72ecc06199f9dda5ede1ea7938bbd36e35e58
FullFaceRub <danielhbury@gmail.com>
2018-02-07 16:26:22 -0700

    finished afternoon project

--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,14 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
-  
+angular.module('devmtnTravel').service('mainSrvc', function ($http) {
+
+    this.getTravelInfo = function () {
+        return $http.get('https://practiceapi.devmountain.com/api/travel/travel-info').then(resp => {
+            return resp
+        })
+    }
+
+    this.getPackageInfo = function () {
+        return $http.get('https://practiceapi.devmountain.com/api/travel/package-info').then(resp => {
+            return resp
+        })
+    }
 });
